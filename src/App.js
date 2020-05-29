fa298916b6af78e04cc0fb0bcc15c12df2182cd7
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-29 16:13:04 -0300

    Disable button and textarea

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const STARTING_TIME = 10;
   const [textTyped, setTextTyped] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [gameStart, setGameStart] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   const handleChange = (event) => {
     const { value } = event.target;
@@ -15,25 +17,38 @@ function App() {
     return wordsArr.filter((word) => word !== "").length;
   };
 
+  const startGame = () => {
+    setGameStart(true);
+    setTimeRemaining(STARTING_TIME);
+    setTextTyped("");
+  };
+
+  const endGame = () => {
+    setGameStart(false);
+    setWordCount(calculateWordCount(textTyped));
+  };
+
   useEffect(() => {
     if (timeRemaining > 0 && gameStart) {
+      setWordCount(0);
       setTimeout(() => {
         setTimeRemaining((prevState) => prevState - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-      setGameStart(false);
-      calculateWordCount(textTyped);
+      endGame();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameStart, timeRemaining]);
 
   return (
     <div className="App">
       <h1>Speed typing game</h1>
-      <textarea onChange={handleChange} value={textTyped} />
+      <textarea disabled={!gameStart} onChange={handleChange} value={textTyped} />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setGameStart(true)}>Start</button>
-      <h4>Word Count: {calculateWordCount(textTyped)}</h4>
+      <button disabled={gameStart} onClick={startGame}>
+        Start
+      </button>
+      <h4>Word Count: {wordCount}</h4>
     </div>
   );
 }
