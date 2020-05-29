5518e0e2c3000b163a544e73fee6ee5f1f0b0fc3
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-29 11:31:38 -0300

    Start layout of the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function App() {
+  const [textTyped, setTextTyped] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setTextTyped(value);
+  };
+
+  console.log(textTyped)
+
   return (
     <div className="App">
-      <h1>Hello Pedro</h1>
+      <h1>Speed typing game</h1>
+      <textarea onChange={handleChange} value={textTyped}/>
+      <h4>Time Remaining</h4>
+      <button>Start</button>
+      <h1>Word Count</h1>
     </div>
   );
 }
