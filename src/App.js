96ecfeac9834ff8028624009f399b99aa1d18ad6
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-05-29 13:08:36 -0300

    Finish word out function and time remaining function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [textTyped, setTextTyped] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [gameStart, setGameStart] = useState(false);
 
   const handleChange = (event) => {
     const { value } = event.target;
     setTextTyped(value);
   };
 
-  console.log(textTyped)
+  const calculateWordCount = (textTyped) => {
+    const wordsArr = textTyped.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  };
+
+  useEffect(() => {
+    if (timeRemaining > 0 && gameStart) {
+      setTimeout(() => {
+        setTimeRemaining((prevState) => prevState - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      setGameStart(false);
+      calculateWordCount(textTyped);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameStart, timeRemaining]);
 
   return (
     <div className="App">
       <h1>Speed typing game</h1>
-      <textarea onChange={handleChange} value={textTyped}/>
-      <h4>Time Remaining</h4>
-      <button>Start</button>
-      <h1>Word Count</h1>
+      <textarea onChange={handleChange} value={textTyped} />
+      <h4>Time Remaining: {timeRemaining}</h4>
+      <button onClick={() => setGameStart(true)}>Start</button>
+      <h4>Word Count: {calculateWordCount(textTyped)}</h4>
     </div>
   );
 }
