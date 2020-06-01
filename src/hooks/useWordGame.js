66238a83a551dc233e5e80c88c414fdefcd5122d
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-01 15:29:48 -0300

    Update hooks on App.js

--- src/hooks/useWordGame.js
+++ src/hooks/useWordGame.js
@@ -1,5 +1,75 @@
-const useWordGame = () => {
+import React, { useState, useEffect, useRef } from "react";
 
-}
+const useWordGame = () => {
+  const STARTING_TIME = 20;
+  const [textTyped, setTextTyped] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [gameStart, setGameStart] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const inputRef = useRef(null);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setTextTyped(value);
+  };
+
+  const blockPaste = (event) => {
+    event.target.addEventListener("paste", (e) => {
+      e.preventDefault();
+      return false;
+    });
+  };
+
+  const calculateWordCount = (textTyped) => {
+    const wordsArr = textTyped.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  };
+
+  const calculateHighScore = () => {
+    if (wordCount > highScore) {
+      setHighScore(wordCount);
+    }
+  };
+
+  const startGame = () => {
+    setGameStart(true);
+    setTimeRemaining(STARTING_TIME);
+    setTextTyped("");
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  };
+
+  const endGame = () => {
+    setGameStart(false);
+    setWordCount(calculateWordCount(textTyped));
+    calculateHighScore();
+  };
+
+  useEffect(() => {
+    if (timeRemaining > 0 && gameStart) {
+      setWordCount(0);
+      setTimeout(() => {
+        setTimeRemaining((prevState) => prevState - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameStart, timeRemaining]);
+
+  return {
+    inputRef,
+    gameStart,
+    handleChange,
+    textTyped,
+    blockPaste,
+    timeRemaining,
+    wordCount,
+    highScore,
+    startGame
+  }
+};
 
 export default useWordGame;
